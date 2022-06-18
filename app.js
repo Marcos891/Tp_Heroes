ace47ed04825f1d40f132414a8125604e60ae6b9
Marcos891 <MarcosMansilla1989@hotmail.com>
2022-06-18 19:45:36 -0300

    Base html

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.get('/home', (req,res) => res.sendFile(path.join(__dirname, '/views', 'index.html')));
+app.get('/babbage', (req,res) => res.sendFile(path.join(__dirname, '/views', 'babbage.html')));
+app.get('/berners-lee', (req,res) => res.sendFile(path.join(__dirname, '/views', 'berners-lee.html')));
+app.get('/clarke', (req,res) => res.sendFile(path.join(__dirname, '/views', 'clarke.html')));
+app.get('/hamilton', (req,res) => res.sendFile(path.join(__dirname, '/views', 'hamilton.html')));
+app.get('/lovelace', (req,res) => res.sendFile(path.join(__dirname, '/views', 'lovelace.html')));
+app.get('/turing', (req,res) => res.sendFile(path.join(__dirname, '/views', 'turing.html')));
+
+
+
+app.listen(port, () => console.log(`servidor corriendo en localHost:${port}`));
